62286177eaeeeedf37de2361bec4995fad564128
Andrii Olkhovych <the.def4alt@gmail.com>
2022-08-08 13:06:55 +0200

    Fix "hello spam" issue
    Fix hello spam by setting a timer and calling
    greet_people function only every 2 seconds

--- src/main.rs
+++ src/main.rs
@@ -8,24 +8,42 @@ struct Name(String);
 
 pub struct HelloPlugin;
 
+struct GreetTimer(Timer);
+
 impl Plugin for HelloPlugin {
     fn build(&self, app: &mut App) {
-       app.add_startup_system(add_people).add_system(greet_people); 
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
+            .add_startup_system(add_people)
+            .add_system(greet_people);
     }
 }
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).add_plugin(HelloPlugin).run();
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugin(HelloPlugin)
+        .run();
 }
 
 fn add_people(mut commands: Commands) {
-    commands.spawn().insert(Person).insert(Name("John Weak".to_string()));
-    commands.spawn().insert(Person).insert(Name("Rebecka Holmes".to_string()));
-    commands.spawn().insert(Person).insert(Name("Sam Topkins".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("John Weak".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Rebecka Holmes".to_string()));
+    commands
+        .spawn()
+        .insert(Person)
+        .insert(Name("Sam Topkins".to_string()));
 }
 
-fn greet_people(query: Query<&Name, With<Person>>) {
-    for name in query.iter() {
-        println!("Hello {}!", name.0);
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in query.iter() {
+            println!("Hello {}!", name.0);
+        }
     }
 }
