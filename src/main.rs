0607475d6cbf76078da7913554cc69aa2d21e22d
Andrii Olkhovych <the.def4alt@gmail.com>
2022-08-08 12:47:43 +0200

    Greet three persons with bevy systems

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,23 @@
 use bevy::prelude::*;
 
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
 fn main() {
-    App::new().add_system(hello_world_system).run();
+    App::new().add_startup_system(add_people).add_system(greet_people).run();
 }
 
-fn hello_world_system() {
-    println!("Hello world");
+fn add_people(mut commands: Commands) {
+    commands.spawn().insert(Person).insert(Name("John Weak".to_string()));
+    commands.spawn().insert(Person).insert(Name("Rebecka Holmes".to_string()));
+    commands.spawn().insert(Person).insert(Name("Sam Topkins".to_string()));
+}
+
+fn greet_people(query: Query<&Name, With<Person>>) {
+    for name in query.iter() {
+        println!("Hello {}!", name.0);
+    }
 }
