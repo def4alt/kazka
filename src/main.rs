d44042c8258e682992ac74fc855d6b086a77cef0
Andrii Olkhovych <the.def4alt@gmail.com>
2022-08-08 20:30:52 +0200

    Setup simple 3D scene
    Setup simple 3D scene with plane and cube

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,56 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
+pub struct GamePlugin;
 
-#[derive(Component)]
-struct Name(String);
-
-pub struct HelloPlugin;
-
-struct GreetTimer(Timer);
-
-impl Plugin for HelloPlugin {
+impl Plugin for GamePlugin {
     fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people)
-            .add_system(greet_people);
+        app.add_startup_system(setup).run();
     }
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane { size: 5.0 })),
+        material: materials.add(Color::rgb(0.5, 0.3, 0.2).into()),
+        ..default()
+    });
+
+    commands.spawn_bundle(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+        material: materials.add(Color::rgb(0.3, 0.6, 0.1).into()),
+        transform: Transform::from_xyz(0.0, 0.5, 0.0),
+        ..default()
+    });
+
+    commands.spawn_bundle(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.0,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4.0, 8.0, 4.0),
+        ..default()
+    });
+
+    commands.spawn_bundle(Camera3dBundle {
+        transform: Transform::from_xyz(-2.0, 2.5, 5.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }
 
 fn main() {
     App::new()
+        .insert_resource(WindowDescriptor {
+            title: "Kazka".to_string(),
+            width: 800.0,
+            height: 600.0,
+            ..default()
+        })
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_plugin(GamePlugin)
         .run();
-}
-
-fn add_people(mut commands: Commands) {
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("John Weak".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Rebecka Holmes".to_string()));
-    commands
-        .spawn()
-        .insert(Person)
-        .insert(Name("Sam Topkins".to_string()));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("Hello {}!", name.0);
-        }
-    }
 }
